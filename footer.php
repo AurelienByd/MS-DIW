e23d96404d13b8c49b910bb06e5b369495555102
Aurélien <rubis91999@hotmail.com>
2023-10-11 08:45:33 +0200

    maj

--- footer.php
+++ footer.php
@@ -1,2 +1,8 @@
-<a href="" title="page Facebook" class="lien_facebook"><img src="/ASSETS/images_the_district/145802.png" alt="icône Facebook" width="50" height="50"></a>
-<a href="" title="page Twitter" class="lien_twitter"><img src="/ASSETS/images_the_district/733579.png" alt="icône Twitter" width="50" height="50"></a>
+<div id="divlogos">
+<a href="" title="page Facebook" class="logolien"><img src="/ASSETS/images_the_district/145802.png" alt="icône Facebook" width="50" height="50"></a>
+<a href="" title="page Twitter" class="logolien"><img src="/ASSETS/images_the_district/733579.png" alt="icône Twitter" width="50" height="50"></a>
+</div>
+<div id="divliensrgpd">
+<a href="politique_de_confidentialite.html" title="page politique et confidentialité" class="liensrgpd">Politique de confidentialité</a>
+<a href="mentions_legales.html" title="page mentions légales" class="liensrgpd">Mentions légales</a>
+</div>
